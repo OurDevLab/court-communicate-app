40ad65a9508dfa3ba9f4c5c1d79328057667a168
Mathias007 <mathias.najlepszy@gmail.com>
2024-09-22 00:08:00 +0200

    [BE] Add files indexation for routes, services, controllers and models.

---
+++ backend/api/services/index.ts
@@ -0,0 +1,5 @@
+export { default as CaseService } from "./Case.service";
+export { default as CourtService } from "./Court.service";
+export { default as DepartmentService } from "./Department.service";
+export { default as MessageService } from "./Message.service";
+export { default as UserService } from "./User.service";
