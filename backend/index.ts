cbcd74bada9b6ce869f2be386b39956c9e5e3db3
Mathias007 <mathias.najlepszy@gmail.com>
2024-09-22 00:08:24 +0200

    [BE] Update index.ts core API file.

--- backend/index.ts
+++ backend/index.ts
@@ -1,31 +1,33 @@
 import prisma from "./prisma";
 import express from "express";
 import cors from "cors";
-import bodyParser from 'body-parser';
+import bodyParser from "body-parser";
 
-import * as core from "express-serve-static-core";
+import {
+    CaseRouter,
+    CourtRouter,
+    DepartmentRouter,
+    MessageRouter,
+    TestRouter,
+    UserRouter,
+} from "./api/routes";
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/test', (req: core.Request, res: core.Response) => {
-    res.send({ message: "Hello World "});
-});
-
-app.post('/test/:id', async (req: core.Request, res: core.Response) => {
-    const { id } = req.params;
-
-    console.log(id);
-
-    res.status(201).send({ id: id });
-});
+app.use("/", CaseRouter);
+app.use("/", CourtRouter);
+app.use("/", DepartmentRouter);
+app.use("/", MessageRouter);
+app.use("/", TestRouter);
+app.use("/", UserRouter);
 
 const finishPrismaService = async () => await prisma.$disconnect();
 
 finishPrismaService();
 
 app.listen(5000, () => {
-    console.log('Listening on 5000');
+    console.log("Listening on 5000");
 });
