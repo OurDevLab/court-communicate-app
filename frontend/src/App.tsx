69cc72e8a3844cec01938484f9d792dfe4f29fe5
Mathias007 <mathias.najlepszy@gmail.com>
2024-09-22 23:28:28 +0200

    [FE] Update imports and routing.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,18 @@ import {
     Dashboard,
     AddUserForm,
     UsersPage,
+    AddCaseForm,
+    AddCourtForm,
+    AddDepartmentForm,
+    DeleteCourtButton,
+    DeleteDepartmentButton,
+    DeleteMessageButton,
+    UpdateCourtForm,
+    UpdateMessageForm,
+    UpdateDepartmentForm,
+    CasesPage,
+    CourtsPage,
+    DepartmentsPage,
 } from "./components";
 
 const App: React.FC = () => {
@@ -16,7 +28,42 @@ const App: React.FC = () => {
                 <Route path="/register" element={<Register />} />
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/users" element={<UsersPage />} />
-                <Route path="/add-user" element={<AddUserForm />} />
+                <Route path="/users/add" element={<AddUserForm />} />
+                <Route path="/cases" element={<CasesPage />} />
+                <Route path="/cases/add" element={<AddCaseForm />} />
+                <Route path="/courts" element={<CourtsPage />} />
+                <Route path="/courts/add" element={<AddCourtForm />} />
+                <Route
+                    path="/courts/:id/edit"
+                    element={<UpdateCourtForm courtId={1} />}
+                />
+                <Route
+                    path="/courts/:id/delete"
+                    element={<DeleteCourtButton courtId={1} />}
+                />
+                <Route path="/departments" element={<DepartmentsPage />} />
+                <Route
+                    path="/departments/add"
+                    element={<AddDepartmentForm />}
+                />
+                <Route path="/cases" element={<CasesPage />} />
+                <Route path="/cases/add" element={<AddCaseForm />} />
+                <Route
+                    path="/messages/:id/edit"
+                    element={<UpdateMessageForm messageId={1} />}
+                />
+                <Route
+                    path="/messages/:id/delete"
+                    element={<DeleteMessageButton messageId={1} />}
+                />
+                <Route
+                    path="/departments/:id/edit"
+                    element={<UpdateDepartmentForm departmentId={1} />}
+                />
+                <Route
+                    path="/departments/:id/delete"
+                    element={<DeleteDepartmentButton departmentId={1} />}
+                />
             </Routes>
         </Router>
     );
--- frontend/src/components/index.tsx
+++ frontend/src/components/index.tsx
@@ -1,7 +1,25 @@
+export { default as AddCourtForm } from "./AddUserForm";
+export { default as AddCaseForm } from "./AddUserForm";
+export { default as AddDepartmentForm } from "./AddDepartmentForm";
+export { default as AddMessageForm } from "./AddUserForm";
 export { default as AddUserForm } from "./AddUserForm";
+
+export { default as DeleteCaseButton } from "./DeleteCaseButton";
+export { default as DeleteCourtButton } from "./DeleteCourtButton";
+export { default as DeleteDepartmentButton } from "./DeleteDepartmentButton";
+export { default as DeleteMessageButton } from "./DeleteMessageButton";
+export { default as DeleteUserButton } from "./DeleteUserButton";
+
+export { default as UpdateCaseForm } from "./UpdateCaseForm";
+export { default as UpdateCourtForm } from "./UpdateCourtForm";
+export { default as UpdateDepartmentForm } from "./UpdateDepartmentForm";
+export { default as UpdateMessageForm } from "./UpdateMessageForm";
+export { default as UpdateUserForm } from "./UpdateUserForm";
+
 export { default as Dashboard } from "./Dashboard";
 export { default as Login } from "./Login";
 export { default as Register } from "./Register";
-export { default as UpdateUserForm } from "./UpdateUserForm";
 export { default as UsersPage } from "./UsersPage";
-export { default as DeleteUserButton } from "./DeleteUserButton";
+export { default as CourtsPage } from "./CourtsPage";
+export { default as CasesPage } from "./CasesPage";
+export { default as DepartmentsPage } from "./DepartmentsPage";
