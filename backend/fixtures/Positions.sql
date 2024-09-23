2b131a6d48c5c636e1e42d5cded709255b33d6d7
Mathias007 <mathias.najlepszy@gmail.com>
2024-09-23 21:04:49 +0200

    [DB] Debug fixtures.

--- backend/fixtures/Positions.sql
+++ backend/fixtures/Positions.sql
@@ -1,2 +1,2 @@
 \copy "Position"(id, name, position_type) FROM './fixtures/Positions.csv' DELIMITER ',' CSV HEADER;
-ALTER SEQUENCE "JobPosition_job_position_id_seq" RESTART WITH 20;
+ALTER SEQUENCE "Position_id_seq" RESTART WITH 20;
