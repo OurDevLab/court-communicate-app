093b11044cad069f0ae9899ef53dc7b044d7c1a3
Mathias007 <mathias.najlepszy@gmail.com>
2024-12-02 21:56:02 +0100

    [BE] Add indexation for config directory.

---
+++ backend/config/index.ts
@@ -0,0 +1,6 @@
+export { default as ConfigVariables } from "./ConfigVariables";
+export { default as ServerPaths } from "./ServerPaths";
+export { default as ServerStatuses } from "./ServerStatuses";
+
+import * as ServerMessages from "./ServerMessages";
+export { ServerMessages };
