e4e6db63be14de133c7114463de941c7cf47077f
Mathias007 <mathias.najlepszy@gmail.com>
2024-12-02 21:57:03 +0100

    [BE] Refactor UserRouter.

--- backend/api/routes/User.router.ts
+++ backend/api/routes/User.router.ts
@@ -1,30 +1,17 @@
 import express from "express";
 
-import UserController from "../controllers/User.controller";
+import { ServerPaths } from "../../config";
+import { UserController } from "../controllers";
 
+const { USERS } = ServerPaths;
 const userController = new UserController();
 
 const userRouter = express();
 
-// 1. CRUD dla modelu User (Użytkownik)
-// a) Tworzenie użytkownika:
-
-userRouter.post("/users", userController.addNewUser);
-
-// b) Odczyt wszystkich użytkowników:
-
-userRouter.get("/users", userController.getAllUsers);
-
-// c) Odczyt konkretnego użytkownika:
-
-userRouter.get("/users/:id", userController.getSelectedUser);
-
-// d) Aktualizacja użytkownika:
-
-userRouter.put("/users/:id", userController.updateUser);
-
-// e) Usunięcie użytkownika:
-
-userRouter.delete("/users/:id", userController.removeUser);
+userRouter.post(USERS, userController.addNewUser);
+userRouter.get(USERS, userController.getAllUsers);
+userRouter.get(`${USERS}/:id`, userController.getSelectedUser);
+userRouter.put(`${USERS}/:id`, userController.updateUser);
+userRouter.delete(`${USERS}/:id`, userController.removeUser);
 
 export default userRouter;
