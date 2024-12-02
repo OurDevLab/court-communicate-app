d7b395f32a50cad8d750360392c50aac13bfe0d2
Mathias007 <mathias.najlepszy@gmail.com>
2024-12-02 21:57:51 +0100

    [BE] Refactor CaseRouter.

--- backend/api/routes/Case.router.ts
+++ backend/api/routes/Case.router.ts
@@ -1,30 +1,17 @@
 import express from "express";
 
-import CaseController from "../controllers/Case.controller";
+import { ServerPaths } from "../../config";
+import { CaseController } from "../controllers";
 
+const { CASES } = ServerPaths;
 const caseController = new CaseController();
 
 const caseRouter = express();
 
-// 2. CRUD dla modelu Case (Sprawa)
-// a) Tworzenie sprawy:
-
-caseRouter.post("/cases", caseController.addNewCase);
-
-// b) Odczyt wszystkich spraw:
-
-caseRouter.get("/cases", caseController.getAllCases);
-
-// c) Odczyt konkretnej sprawy:
-
-caseRouter.get("/cases/:id", caseController.getSelectedCase);
-
-// d) Aktualizacja sprawy:
-
-caseRouter.put("/cases/:id", caseController.updateCase);
-
-// e) Usunięcie sprawy:
-
-caseRouter.delete("/cases/:id", caseController.removeCase);
+caseRouter.post(CASES, caseController.addNewCase);
+caseRouter.get(CASES, caseController.getAllCases);
+caseRouter.get(`${CASES}/:id`, caseController.getSelectedCase);
+caseRouter.put(`${CASES}/:id`, caseController.updateCase);
+caseRouter.delete(`${CASES}/:id`, caseController.removeCase);
 
 export default caseRouter;
