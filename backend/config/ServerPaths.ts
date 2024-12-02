f3df44c74a94ba934b4bf8740c2f68edccb321e6
Mathias007 <mathias.najlepszy@gmail.com>
2024-12-02 21:54:53 +0100

    [BE] Add ServerPaths config file.

---
+++ backend/config/ServerPaths.ts
@@ -0,0 +1,17 @@
+const ServerPaths = {
+    ROOT: "/",
+
+    LOGIN: "/login",
+    REGISTER: "/register",
+    PROTECTED: "/protected",
+
+    CASES: "/cases",
+    COURTS: "/courts",
+    DEPARTMENTS: "/departments",
+    MESSAGES: "/messages",
+    UPLOADS: "/uploads",
+    TEST: "/test",
+    USERS: "/users",
+};
+
+export default ServerPaths;
