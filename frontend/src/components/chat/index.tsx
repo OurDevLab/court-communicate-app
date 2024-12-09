568a88e989d32e66f7f2ea2b3550109dae14b5b6
Mathias007 <mathias.najlepszy@gmail.com>
2024-12-09 21:40:21 +0100

    [FE] Update styles.

---
+++ frontend/src/components/chat/index.tsx
@@ -0,0 +1,6 @@
+export { default as Chat } from "./Chat";
+export { default as ChatEmptyContent } from "./ChatEmptyContent";
+export { default as ChatForm } from "./ChatForm";
+export { default as ChatMessageBox } from "./ChatMessageBox";
+export { default as SidebarContent } from "./SidebarContent";
+export { default as SidebarFooter } from "./SidebarFooter";
