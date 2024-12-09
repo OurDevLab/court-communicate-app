265dc7b0abab9e7137a76a7b39e004487a8ebdce
Mathias007 <mathias.najlepszy@gmail.com>
2024-12-09 21:39:47 +0100

    [FE] Add SidebarContent component.

---
+++ frontend/src/components/chat/SidebarContent.tsx
@@ -0,0 +1,36 @@
+const SidebarContent = ({
+    cases,
+    selectedCaseId,
+    setSelectedCaseId,
+}: {
+    cases: { id: number; name: string }[];
+    selectedCaseId: number | null;
+    setSelectedCaseId: (id: number | null) => void;
+}) => {
+    return (
+        <div className="toggle-section">
+            <div className="toggle-header">
+                <h3>Twoje sprawy</h3>
+            </div>
+            <div className="toggle-content">
+                <ul className="list">
+                    {cases.map((caseItem) => (
+                        <li
+                            key={caseItem.id}
+                            className={`list-item ${
+                                selectedCaseId === caseItem.id
+                                    ? "list-item-selected"
+                                    : "list-item-default"
+                            }`}
+                            onClick={() => setSelectedCaseId(caseItem.id)}
+                        >
+                            {caseItem.name}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default SidebarContent;
