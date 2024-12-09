67a7092449a04e227d25f7c7a9e7f6410f645842
Mathias007 <mathias.najlepszy@gmail.com>
2024-12-09 21:39:40 +0100

    [FE] Add ChatEmptyContent component.

---
+++ frontend/src/components/chat/ChatEmptyContent.tsx
@@ -0,0 +1,19 @@
+const ChatEmptyContent = () => {
+    return (
+        <div className="flex flex-col empty-content-container">
+            <h3 className="empty-content-title">
+                Witaj w aplikacji do komunikacji!
+            </h3>
+            <img
+                // src={SilverPhoenixLogo} todo
+                alt="phoenix-logo"
+                className="empty-content-logo"
+            />
+            <div className="empty-content-subtitle">
+                &larr; Wybierz sprawę z panelu bocznego
+            </div>
+        </div>
+    );
+};
+
+export default ChatEmptyContent;
