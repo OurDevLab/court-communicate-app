07be55b86130cde6d6e0a95d47b62aed60cd60e4
Mathias007 <mathias.najlepszy@gmail.com>
2024-12-14 21:54:49 +0100

    [FE] Update Router inside main App component.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,69 +2,68 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import { Login, Register } from "./components/auth";
-import { CasesPage, AddCaseForm } from "./components/case";
+import { AddCaseForm, CasePreview, UpdateCaseForm } from "./components/case";
 import {
-    CourtsPage,
     AddCourtForm,
+    CourtPreview,
     UpdateCourtForm,
-    DeleteCourtButton,
 } from "./components/court";
+import { AddUserForm, UpdateUserForm, UserPreview } from "./components/user";
 import {
-    DepartmentsPage,
     AddDepartmentForm,
     UpdateDepartmentForm,
-    DeleteDepartmentButton,
 } from "./components/department";
-import { UpdateMessageForm, DeleteMessageButton } from "./components/message";
-import { UsersPage, AddUserForm } from "./components/user";
+
 import { Chat } from "./components/chat";
-import { Dashboard } from "./components/dashboard";
+
+import {
+    CasesList,
+    CourtsList,
+    Dashboard,
+    DocumentsPanel,
+    UsersList,
+} from "./components/dashboard";
+import DepartmentsList from "./components/dashboard/DepartmentsList";
 
 const App: React.FC = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Dashboard />} />
+                <Route path="/courts" element={<CourtsList />} />
+                <Route path="/cases" element={<CasesList />} />
+                <Route path="/users" element={<UsersList />} />
+                <Route path="/documents" element={<DocumentsPanel />} />
+                <Route
+                    path="/courts/:id/departments"
+                    element={<DepartmentsList />}
+                />
                 <Route path="/chat" element={<Chat />} />
+
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
-                <Route path="/users" element={<UsersPage />} />
+
                 <Route path="/users/add" element={<AddUserForm />} />
-                <Route path="/cases" element={<CasesPage />} />
                 <Route path="/cases/add" element={<AddCaseForm />} />
-                <Route path="/courts" element={<CourtsPage />} />
                 <Route path="/courts/add" element={<AddCourtForm />} />
-                <Route
-                    path="/courts/:id/edit"
-                    element={<UpdateCourtForm courtId={1} />}
-                />
-                <Route
-                    path="/courts/:id/delete"
-                    element={<DeleteCourtButton courtId={1} />}
-                />
-                <Route path="/departments" element={<DepartmentsPage />} />
+
                 <Route
                     path="/departments/add"
                     element={<AddDepartmentForm />}
                 />
-                <Route path="/cases" element={<CasesPage />} />
-                <Route path="/cases/add" element={<AddCaseForm />} />
+
+                <Route path="/cases/edit/:id" element={<UpdateCaseForm />} />
+                <Route path="/courts/:id" element={<UpdateCourtForm />} />
+
                 <Route
-                    path="/messages/:id/edit"
-                    element={<UpdateMessageForm messageId={1} />}
+                    path="/departments/edit/:id"
+                    element={<UpdateDepartmentForm />}
                 />
-                <Route
-                    path="/messages/:id/delete"
-                    element={<DeleteMessageButton messageId={1} />}
-                />
-                <Route
-                    path="/departments/:id/edit"
-                    element={<UpdateDepartmentForm departmentId={1} />}
-                />
-                <Route
-                    path="/departments/:id/delete"
-                    element={<DeleteDepartmentButton departmentId={1} />}
-                />
+                <Route path="/users/edit/:id" element={<UpdateUserForm />} />
+
+                <Route path="/court/preview/:id" element={<CourtPreview />} />
+                <Route path="/case/preview/:id" element={<CasePreview />} />
+                <Route path="/user/preview/:id" element={<UserPreview />} />
             </Routes>
         </Router>
     );
