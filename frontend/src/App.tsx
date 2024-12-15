2c0009d8db0bf89707abe225ef9be58e65e8ce37
Mathias007 <mathias.najlepszy@gmail.com>
2024-12-15 20:04:08 +0100

    [FE] Use UserContextProvider in App router.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -36,57 +36,60 @@ import {
 // } from "./components/document";
 
 import { PrivateRoute } from ".";
+import { UserContextProvider } from "./context/User.context";
 
 const App: React.FC = () => {
     return (
-        <Router>
-            <Routes>
+        <UserContextProvider>
+            <Router>
+                <Routes>
 
-            <Route path="/login" element={<LoginForm />} />
-            <Route path="/register" element={<RegisterForm />} />
+                <Route path="/login" element={<LoginForm />} />
+                <Route path="/register" element={<RegisterForm />} />
 
-            <Route element={<PrivateRoute />}>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/courts" element={<CourtsList />} />
-                <Route path="/cases" element={<CasesList />} />
-                <Route path="/users" element={<UsersList />} />
-                <Route path="/documents" element={<DocumentsPanel />} />
-                <Route
-                    path="/courts/:id/departments"
-                    element={<DepartmentsList />}
-                />
-                <Route path="/chat" element={<Chat />} />
+                <Route element={<PrivateRoute />}>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/courts" element={<CourtsList />} />
+                    <Route path="/cases" element={<CasesList />} />
+                    <Route path="/users" element={<UsersList />} />
+                    <Route path="/documents" element={<DocumentsPanel />} />
+                    <Route
+                        path="/courts/:id/departments"
+                        element={<DepartmentsList />}
+                    />
+                    <Route path="/chat" element={<Chat />} />
 
-                <Route path="/users/add" element={<AddUserForm />} />
-                <Route path="/cases/add" element={<AddCaseForm />} />
-                <Route path="/courts/add" element={<AddCourtForm />} />
+                    <Route path="/users/add" element={<AddUserForm />} />
+                    <Route path="/cases/add" element={<AddCaseForm />} />
+                    <Route path="/courts/add" element={<AddCourtForm />} />
 
-                <Route
-                    path="/departments/add"
-                    element={<AddDepartmentForm />}
-                />
+                    <Route
+                        path="/departments/add"
+                        element={<AddDepartmentForm />}
+                    />
 
-                <Route path="/cases/edit/:id" element={<UpdateCaseForm />} />
-                <Route path="/courts/edit/:id" element={<UpdateCourtForm />} />
+                    <Route path="/cases/edit/:id" element={<UpdateCaseForm />} />
+                    <Route path="/courts/edit/:id" element={<UpdateCourtForm />} />
 
-                <Route
-                    path="/departments/edit/:id"
-                    element={<UpdateDepartmentForm />}
-                />
-                <Route path="/users/edit/:id" element={<UpdateUserForm />} />
+                    <Route
+                        path="/departments/edit/:id"
+                        element={<UpdateDepartmentForm />}
+                    />
+                    <Route path="/users/edit/:id" element={<UpdateUserForm />} />
 
-                <Route path="/courts/preview/:id" element={<CourtPreview />} />
-                <Route path="/cases/preview/:id" element={<CasePreview />} />
-                <Route path="/users/preview/:id" element={<UserPreview />} />
+                    <Route path="/courts/preview/:id" element={<CourtPreview />} />
+                    <Route path="/cases/preview/:id" element={<CasePreview />} />
+                    <Route path="/users/preview/:id" element={<UserPreview />} />
 
-                {/* <Route path="/documents/cassation/add/" element={<AddCassationForm />} />
-                <Route path="/documents/cassation/edit/" element={<AddCassationForm />} />
-                <Route path="/documents/complaint/add" element={<ComplaintForm />} />
-                <Route path="/documents/judgment/add" element={<JudgmentForm />} />
-                <Route path="/documents/ordinance/add" element={<OrdinanceForm />} /> */}
-            </Route>
-            </Routes>
-        </Router>
+                    {/* <Route path="/documents/cassation/add/" element={<AddCassationForm />} />
+                    <Route path="/documents/cassation/edit/" element={<AddCassationForm />} />
+                    <Route path="/documents/complaint/add" element={<ComplaintForm />} />
+                    <Route path="/documents/judgment/add" element={<JudgmentForm />} />
+                    <Route path="/documents/ordinance/add" element={<OrdinanceForm />} /> */}
+                </Route>
+                </Routes>
+            </Router>
+        </UserContextProvider>
     );
 };
 
