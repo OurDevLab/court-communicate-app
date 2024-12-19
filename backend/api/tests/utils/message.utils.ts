0883c7b0bed6164989905e769c799739df3738da
Mathias007 <mathias.najlepszy@gmail.com>
2024-12-19 21:01:27 +0100

    [BE] Update Message unit test.

--- backend/api/tests/utils/message.utils.ts
+++ backend/api/tests/utils/message.utils.ts
@@ -2,18 +2,18 @@ import { MessageModel } from "../../models";
 
 export const mockMessage: MessageModel.Message = {
     id: 1,
-    caseId: 1,
-    recipientId: 2,
-    senderId: 1,
-    text: "Hello, this is a test message.",
+    text: "New test message",
     file: null,
     timestamp: new Date(),
+    case: { case_id: 1 },
+    sender: { user_id: 1 },
+    recipient: { user_id: 2 },
 };
 
-export const mockMessageCreateInput: MessageModel.CreateMessage = {
+export const mockMessageCreateInput = {
     caseId: 1,
-    recipientId: 2,
     senderId: 1,
+    recipientId: 1,
     text: "New test message",
     file: null,
 };
