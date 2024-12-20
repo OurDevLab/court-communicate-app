8232ce0b6e1a4f6bfb69393c6712b4bd2cb07d7c
Mathias007 <mathias.najlepszy@gmail.com>
2024-12-20 22:01:42 +0100

    [FE] Update and extend ComplaintForm.

--- frontend/src/components/document/ComplaintForm.tsx
+++ frontend/src/components/document/ComplaintForm.tsx
@@ -1,23 +1,90 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { UserContext } from "../../context/User.context";
 import api from "../../api";
+import { Navigation } from "../dashboard";
+import { useNavigate } from "react-router-dom";
 
 const ComplaintForm: React.FC = () => {
+    const navigate = useNavigate();
+
     const { id: userId } = useContext(UserContext);
+    const [courts, setCourts] = useState([]);
     const [formData, setFormData] = useState({
-        caseId: "",
         date: "",
+        location: "",
+        courtId: "",
+        organ: "",
         complainant: "",
-        complainantAddress: "",
-        complainantKRS: "",
-        complaintContent: "",
+        proxy: "",
+        complainantType: "",
+        pesel: "",
+        krs: "",
+        nipOrRegon: "",
+        attorney: "",
+        subjectOfComplaint: "",
+        specificAct: "",
+        valueOfSubject: "",
+        feeAmount: "",
+        feeType: "",
+        legalInterest: "",
+        urgencyDetails: "",
+        scopeOfComplaint: "whole",
+        charges: "",
+        demands: "",
+        justification: "",
+        attachments: "",
+    });
+    const [dynamicFields, setDynamicFields] = useState({
+        showPesel: false,
+        showKrs: false,
+        showNipOrRegon: false,
+        showValueField: false,
+        showLegalInterestField: false,
+        showUrgencyField: false,
     });
 
+    useEffect(() => {
+        const fetchCourts = async () => {
+            try {
+                const response = await api.get("/courts");
+                setCourts(response.data);
+            } catch (error) {
+                console.error("Błąd podczas pobierania listy sądów", error);
+            }
+        };
+
+        fetchCourts();
+    }, []);
+
     const handleChange = (
-        e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+        e: React.ChangeEvent<
+            HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+        >
     ) => {
         const { name, value } = e.target;
         setFormData({ ...formData, [name]: value });
+
+        if (name === "complainantType") {
+            setDynamicFields({
+                ...dynamicFields,
+                showPesel: value === "individual",
+                showKrs: value === "legalEntity",
+                showNipOrRegon: value === "otherUnit",
+            });
+        }
+
+        if (name === "subjectOfComplaint") {
+            setDynamicFields({
+                ...dynamicFields,
+                showValueField: value === "decision",
+                showLegalInterestField: [
+                    "actOfLocalLaw",
+                    "actOfSupervision",
+                    "otherSelfGoverningAct",
+                ].includes(value),
+                showUrgencyField: value === "delayOrLackOfAction",
+            });
+        }
     };
 
     const handleSubmit = async (e: React.FormEvent) => {
@@ -26,9 +93,9 @@ const ComplaintForm: React.FC = () => {
             alert("Nie jesteś zalogowany.");
             return;
         }
+
         try {
             const payload = {
-                caseId: formData.caseId,
                 userId,
                 type: "complaint",
                 content: formData,
@@ -46,6 +113,9 @@ const ComplaintForm: React.FC = () => {
 
     return (
         <form className="form-container" onSubmit={handleSubmit}>
+            <Navigation />
+            <h1 className="header">Formularz skargi</h1>
+
             <div className="form-group">
                 <label htmlFor="date">Data</label>
                 <input
@@ -55,6 +125,53 @@ const ComplaintForm: React.FC = () => {
                     value={formData.date}
                     onChange={handleChange}
                     className="form-input"
+                    required
+                />
+            </div>
+            <div className="form-group">
+                <label htmlFor="location">Miejscowość</label>
+                <input
+                    id="location"
+                    name="location"
+                    type="text"
+                    value={formData.location}
+                    onChange={handleChange}
+                    className="form-input"
+                    placeholder="Wpisz miejscowość"
+                    required
+                />
+            </div>
+
+            <div className="form-group">
+                <label htmlFor="courtId">Sąd</label>
+                <select
+                    id="courtId"
+                    name="courtId"
+                    value={formData.courtId}
+                    onChange={handleChange}
+                    className="form-input"
+                    required
+                >
+                    <option value="">Wybierz sąd</option>
+                    {courts.map((court) => (
+                        <option key={court.id} value={court.id}>
+                            {court.name}
+                        </option>
+                    ))}
+                </select>
+            </div>
+
+            <div className="form-group">
+                <label htmlFor="organ">Organ</label>
+                <input
+                    id="organ"
+                    name="organ"
+                    type="text"
+                    value={formData.organ}
+                    onChange={handleChange}
+                    className="form-input"
+                    placeholder="Wpisz oznaczenie organu"
+                    required
                 />
             </div>
 
@@ -67,52 +184,235 @@ const ComplaintForm: React.FC = () => {
                     value={formData.complainant}
                     onChange={handleChange}
                     className="form-input"
-                    placeholder="Nazwa Skarżącego"
+                    placeholder="Wpisz oznaczenie skarżącego"
+                    required
                 />
             </div>
-
             <div className="form-group">
-                <label htmlFor="complainantAddress">Adres Skarżącego</label>
+                <label htmlFor="proxy">Pełnomocnik</label>
                 <input
-                    id="complainantAddress"
-                    name="complainantAddress"
+                    id="proxy"
+                    name="proxy"
                     type="text"
-                    value={formData.complainantAddress}
+                    value={formData.proxy}
                     onChange={handleChange}
                     className="form-input"
-                    placeholder="Adres"
+                    placeholder="Podaj dane pełnomocnika skarżącego"
                 />
             </div>
-
             <div className="form-group">
-                <label htmlFor="complainantKRS">KRS Skarżącego</label>
-                <input
-                    id="complainantKRS"
-                    name="complainantKRS"
-                    type="text"
-                    value={formData.complainantKRS}
+                <label htmlFor="complainantType">Podmiotowość</label>
+                <select
+                    id="complainantType"
+                    name="complainantType"
+                    value={formData.complainantType}
                     onChange={handleChange}
                     className="form-input"
-                    placeholder="KRS"
-                />
+                    required
+                >
+                    <option value="">
+                        Określ podmiotowość prawną skarżącego
+                    </option>
+                    <option value="individual">Osoba fizyczna</option>
+                    <option value="legalEntity">Osoba prawna</option>
+                    <option value="otherUnit">
+                        Inna jednostka organizacyjna
+                    </option>
+                </select>
+            </div>
+
+            {dynamicFields.showPesel && (
+                <div className="form-group">
+                    <label htmlFor="pesel">PESEL</label>
+                    <input
+                        id="pesel"
+                        name="pesel"
+                        type="text"
+                        value={formData.pesel}
+                        onChange={handleChange}
+                        className="form-input"
+                        placeholder="Wpisz numer PESEL"
+                        required
+                    />
+                </div>
+            )}
+            {dynamicFields.showKrs && (
+                <div className="form-group">
+                    <label htmlFor="krs">KRS</label>
+                    <input
+                        id="krs"
+                        name="krs"
+                        type="text"
+                        value={formData.krs}
+                        onChange={handleChange}
+                        className="form-input"
+                        placeholder="Wpisz numer KRS"
+                        required
+                    />
+                </div>
+            )}
+            {dynamicFields.showNipOrRegon && (
+                <div className="form-group">
+                    <label htmlFor="nipOrRegon">NIP/REGON</label>
+                    <input
+                        id="nipOrRegon"
+                        name="nipOrRegon"
+                        type="text"
+                        value={formData.nipOrRegon}
+                        onChange={handleChange}
+                        className="form-input"
+                        placeholder="Wpisz numer NIP lub REGON"
+                        required
+                    />
+                </div>
+            )}
+
+            <div className="form-group">
+                <label htmlFor="subjectOfComplaint">
+                    Przedmiot zaskarżenia
+                </label>
+                <select
+                    id="subjectOfComplaint"
+                    name="subjectOfComplaint"
+                    value={formData.subjectOfComplaint}
+                    onChange={handleChange}
+                    className="form-input"
+                    required
+                >
+                    <option value="">Wybierz przedmiot</option>
+                    <option value="decision">Decyzja</option>
+                    <option value="resolution">Postanowienie</option>
+                    <option value="actOfLocalLaw">Akt prawa miejscowego</option>
+                    <option value="actOfSupervision">Akt nadzoru</option>
+                    <option value="delayOrLackOfAction">
+                        Bezczynność lub przewlekłość
+                    </option>
+                </select>
+            </div>
+
+            {dynamicFields.showValueField && (
+                <div className="form-group">
+                    <label htmlFor="valueOfSubject">
+                        Wartość przedmiotu zaskarżenia
+                    </label>
+                    <input
+                        id="valueOfSubject"
+                        name="valueOfSubject"
+                        type="number"
+                        value={formData.valueOfSubject}
+                        onChange={handleChange}
+                        className="form-input"
+                        placeholder="Wpisz wartość"
+                    />
+                </div>
+            )}
+
+            {dynamicFields.showLegalInterestField && (
+                <div className="form-group">
+                    <label htmlFor="legalInterest">Interes prawny</label>
+                    <textarea
+                        id="legalInterest"
+                        name="legalInterest"
+                        value={formData.legalInterest}
+                        onChange={handleChange}
+                        className="form-input"
+                        rows={3}
+                        placeholder="Opisz interes prawny w zaskarżeniu"
+                    ></textarea>
+                </div>
+            )}
+
+            {dynamicFields.showUrgencyField && (
+                <div className="form-group">
+                    <label htmlFor="urgencyDetails">Ponaglenie</label>
+                    <textarea
+                        id="urgencyDetails"
+                        name="urgencyDetails"
+                        value={formData.urgencyDetails}
+                        onChange={handleChange}
+                        className="form-input"
+                        rows={3}
+                        placeholder="Opisz szczegóły uprzedniego ponaglenia"
+                    ></textarea>
+                </div>
+            )}
+
+            <div className="form-group">
+                <label htmlFor="scopeOfComplaint">Zakres zaskarżenia</label>
+                <select
+                    id="scopeOfComplaint"
+                    name="scopeOfComplaint"
+                    value={formData.scopeOfComplaint}
+                    onChange={handleChange}
+                    className="form-input"
+                >
+                    <option value="whole">W całości</option>
+                    <option value="partial">W części</option>
+                </select>
             </div>
 
             <div className="form-group">
-                <label htmlFor="complaintContent">Treść skargi</label>
+                <label htmlFor="charges">Zarzuty</label>
                 <textarea
-                    id="complaintContent"
-                    name="complaintContent"
-                    value={formData.complaintContent}
+                    id="charges"
+                    name="charges"
+                    value={formData.charges}
                     onChange={handleChange}
                     className="form-input"
                     rows={4}
-                    placeholder="Opisz treść skargi"
+                    placeholder="Wpisz zarzuty"
+                ></textarea>
+            </div>
+
+            <div className="form-group">
+                <label htmlFor="demands">Wnioski (żądania)</label>
+                <textarea
+                    id="demands"
+                    name="demands"
+                    value={formData.demands}
+                    onChange={handleChange}
+                    className="form-input"
+                    rows={4}
+                    placeholder="Wpisz wnioski"
+                ></textarea>
+            </div>
+
+            <div className="form-group">
+                <label htmlFor="justification">Uzasadnienie</label>
+                <textarea
+                    id="justification"
+                    name="justification"
+                    value={formData.justification}
+                    onChange={handleChange}
+                    className="form-input"
+                    rows={6}
+                    placeholder="Wpisz uzasadnienie skargi"
+                ></textarea>
+            </div>
+
+            <div className="form-group">
+                <label htmlFor="attachments">Załączniki</label>
+                <textarea
+                    id="attachments"
+                    name="attachments"
+                    value={formData.attachments}
+                    onChange={handleChange}
+                    className="form-input"
+                    rows={3}
+                    placeholder="Wymień załączniki"
                 ></textarea>
             </div>
 
             <div className="form-buttons-group">
                 <button type="submit" className="form-button">
-                    Wyślij
+                    Wyślij skargę
+                </button>
+
+                <button
+                    onClick={() => navigate("/documents")}
+                    className="form-button form-button-cancel"
+                >
+                    Anuluj
                 </button>
             </div>
         </form>
