c5ce773b79aec26e1c8c9884ad67cc02add6d24a
Mathias007 <mathias.najlepszy@gmail.com>
2024-12-21 20:24:59 +0100

    [FE] Add RoutesPaths config file.

---
+++ frontend/src/config/RoutesPaths.ts
@@ -0,0 +1,23 @@
+const RoutesPaths = {
+    LOGIN: "/login",
+    REGISTER: "/register",
+    DASHBOARD: "/",
+    COURTS: "/courts",
+    CASES: "/cases",
+    USERS: "/users",
+    DOCUMENTS: "/documents",
+    DEPARTMENTS: "departments",
+    CHAT: "/chat",
+
+    ADD: "/add",
+    EDIT: "/edit",
+    PREVIEW: "/preview",
+    ID_PARAM: "/:id",
+
+    CASSATION: "/cassation",
+    COMPLAINT: "/complaint",
+    JUDGMENT: "/judgment",
+    ORDINANCE: "/ordinance",
+};
+
+export default RoutesPaths;
